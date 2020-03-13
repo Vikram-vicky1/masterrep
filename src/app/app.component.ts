0061f25b266a9135263a5fb76cb1abfd3040c575
LOKESH <lokesh.babu@blocmatrix.com>
2020-03-13 19:19:50 +0530

    modified some pipe data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes-start';
+  servers : any = [
+    {
+      instanceType : 'medium',
+      name : 'production server',
+      status : 'stable',
+      started : new Date(15,1,2017)
+    },
+    {
+      instanceType : 'large',
+      name : 'User database',
+      status : 'critical',
+      started : new Date(15,1,2017)
+    },
+    {
+      instanceType : 'small',
+      name : 'development server',
+      status : 'offline',
+      started : new Date(15,1,2017)
+    },
+    {
+      instanceType : 'large',
+      name : 'testing environment server',
+      status : 'critical',
+      started : new Date(15,1,2017)
+    }
+  ];
+
+  getStatusClasses(server : {instnceType : string, name : string, status : string, started : Date}){
+    return {
+      'list-group-item list-group-item-action list-group-item-success': server.status === 'stable',
+      'list-group-item list-group-item-action list-group-item-warning': server.status === 'offline',
+      'list-group-item list-group-item-action list-group-item-danger' : server.status === 'critical'
+    }
+  }
 }
